7a817904dd75bb704b21b2a2a2d1b8c4c022aadd
Corey Carter <crc2231@gmail.com>
2020-10-19 22:10:48 -0600

    Comment out before_save due to double calculation of unit price

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :merchant
-  before_save { |item| item.unit_price = calculate_price }
+  # validates_presence_of :id, :name, :description, :unit_price, :merchant_id
+  # before_save { |item| item.unit_price = calculate_price }
 
   private
 
-  def calculate_price
-    (unit_price / 100).to_f
-  end
+  # def calculate_price
+  #   (unit_price / 100).to_f
+  # end
 
 end
