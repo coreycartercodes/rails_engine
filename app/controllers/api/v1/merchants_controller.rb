3418881a5e63ddbc51dca71bdcb95bde56940072
Corey Carter <crc2231@gmail.com>
2020-10-19 22:11:14 -0600

    Add serializers and format controller to implement

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,24 +1,25 @@
 class Api::V1::MerchantsController < ApplicationController
 
   def index
-    render json: Merchant.all
+    render json: MerchantSerializer.new(Merchant.all)
   end
 
   def show
-    render json: Merchant.find(params[:id])
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
 
   def create
     ActiveRecord::Base.connection.reset_pk_sequence!('merchants')
-    render json: Merchant.create(merchant_params)
+    new_merchant = Merchant.new(merchant_params)
+    render json: MerchantSerializer.new(new_merchant) if new_merchant.save
   end
 
   def update
-    render json: Merchant.update(params[:id], merchant_params)
+    render json: MerchantSerializer.new(Merchant.update(params[:id], merchant_params))
   end
 
   def destroy
-    render json: Merchant.delete(params[:id])
+    render json: Merchant.destroy(params[:id])
   end
 
   private
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -10,13 +10,13 @@ describe 'New API' do
 
     merchants = JSON.parse(response.body, symbolize_names: true)
     
-    expect(merchants.count).to eq(3)
+    expect(merchants[:data].count).to eq(3)
 
-    merchants.each do |merchant|
+    merchants[:data].each do |merchant|
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
+      expect(merchant[:id]).to be_an(String)
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
 
@@ -29,11 +29,11 @@ describe 'New API' do
   
     expect(response).to be_successful
   
-    expect(merchant).to have_key(:id)
-    expect(merchant[:id]).to eq(id)
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data][:id]).to eq(id.to_s)
   
-    expect(merchant).to have_key(:name)
-    expect(merchant[:name]).to be_a(String)
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
   end
 
   it 'Can create an merchant' do
