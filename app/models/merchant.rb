275c0690fe5fcf5fff8e99024f67d9443ee0b6ed
Corey Carter <crc2231@gmail.com>
2020-10-22 16:28:25 -0600

    Create single/multi find methods

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,19 @@
 class Merchant < ApplicationRecord
   has_many :items
+  has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :transactions, through: :invoices
+
+
+  def self.single_find(attribute, query)
+    Merchant.where("LOWER(#{attribute}) LIKE ?", "%#{query}%").first
+  end
+
+  def self.multi_find(attribute, query)
+    Merchant.where("LOWER(#{attribute}) LIKE ?", "%#{query}%")
+  end
+
+  def self.merchant_revenue(id)
+
+  end
 end
