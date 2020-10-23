29a34e207db733c0105698fc842eebc68d4ad5b2
Corey Carter <crc2231@gmail.com>
2020-10-23 01:43:24 -0600

    Add find_total_revenue to invoice model

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,9 +2,4 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
   validates_presence_of :quantity, :unit_price
-
-
-  def find_total_revenue(start_date, end_date)
-    binding.pry
-  end
 end
